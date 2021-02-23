1ec16a4dde7fe04dbd4b0aab087dce9bd3c8e57a
xavambro <xavambro@gmail.com>
2021-02-23 16:48:08 +0100

    Creacion de componentes

---
+++ src/components/Hijo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>{{message}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Hijo',
+    data:function(){
+        return {
+            message: "Hola, soy el hijo"
+        }
+    },
+  
+}
+</script>
+
+<style scoped>
+    h1{
+        color:green
+    }
+</style>
---
+++ src/components/Padre.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1>{{message}}</h1>
+        <Hijo></Hijo>
+    </div>
+</template>
+
+<script>
+import Hijo from './Hijo.vue'
+
+export default {
+    name: 'Padre',
+    data:function(){
+        return {
+            message: "Hola, soy el padre"
+        }
+    },
+    components:{
+        Hijo
+    }
+  
+}
+</script>
+
+<style scoped>
+    
+</style>
